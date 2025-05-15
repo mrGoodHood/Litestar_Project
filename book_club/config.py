74a7c488654d5883a8b10ed6736f7abf8b245864
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-05-15 23:18:55 +0300

    postgres config

---
+++ book_club/config.py
@@ -0,0 +1,18 @@
+from os import envirob as env
+
+from pydantic import Field, BaseModel
+
+
+class RabbitMQConfig(BaseModel):
+    host: str = Field(alias='RABBITMQ_HOST')
+    port: int = Field(alias='RABBITMQ_PORT')
+    login: str = Field(alias='RABBITMQ_USER')
+    password: str = Field(alias='RABBITMQ_PASS')
+
+
+class PostgresConfig(BaseModel):
+    host: str = Field(alias='POSTGRES_HOST')
+    port: int = Field(alias='POSTGRES_PORT')
+    login: str = Field(alias='POSTGRES_USER')
+    password: str = Field(alias='POSTGRES_PASSWORD')
+    database: str = Field(alias='POSTGRES_DB')
