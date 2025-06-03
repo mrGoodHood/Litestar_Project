153dec55d12d042218873a30b9842f768fc0a972
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-06-03 22:11:17 +0300

    get session

--- book_club/ioc.py
+++ book_club/ioc.py
@@ -30,3 +30,10 @@ def get_session_maker(self, config: Config) -> async_sessionmaker[AsyncSession]:
     return new_session_maker(config.postgres)
 
 
+@provide(scope=Scope.REQUEST)
+async def get_session(self, session_maker: async_sessionmaker[AsyncSession]) -> AsyncIterable[AnyOf[
+    AsyncSession,
+    interfaces.DBSession,
+]]:
+    async with session_maker() as session:
+        yield session
