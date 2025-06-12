fad3126016af2ef7750914217b530538d19e9599
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-06-12 23:50:29 +0300

    main

---
+++ book_club/main.py
@@ -0,0 +1,22 @@
+from dishka import make_async_container
+from dishka.integrations import faststream as faststream_integration
+from dishka.integrations import litestar as litestar_integration
+from faststream import FastStream
+from litestar import Litestar
+
+from book_club.config import Config
+from book_club.controllers.amqp import AMQPBookController
+from book_club.controllers.http import HTTPBookController
+from book_club.infrastructure.broker import new_broker
+from book_club.ioc import AppProvider
+
+config = Config()
+container = make_async_container(AppProvider(), context={Config: config})
+
+
+def get_faststream_app() -> FastStream:
+    broker = new_broker(config.rabbitmq)
+    faststream_app = FastStream(broker)
+    faststream_integration.setup_dishka(container, faststream_app, auto_inject=True)
+    broker.include_router(AMQPBookController)
+    return faststream_app
